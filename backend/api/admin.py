c1254fe3c26bf35bf8031eeff927dada4a3a66e2
Junaid Saleem <junaidsaleem986@gmail.com>
2024-08-18 17:09:17 +0500

    added models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import User,Profile
+from .models import User,Profile,Category,Comment,Post,Notification,Bookmark
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Profile)
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Notification)
+admin.site.register(Comment)
+admin.site.register(Bookmark)
--- backend/api/models.py
+++ backend/api/models.py
@@ -58,3 +58,101 @@ def save_user_profile(sender,instance,**kwargs):
 post_save.connect(create_user_profile,sender=User)
 post_save.connect(save_user_profile,sender=User)
 
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.FileField(upload_to="image",null=True,blank=True)
+    slug = models.SlugField(unique=True,null=True,blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self,*args, **kwargs):
+        if self.slug=="" or self.slug==None:
+            self.slug = slugify(self.title)
+
+        super(Category,self).save(*args, **kwargs)
+    
+    def post_count(self):
+        return Post.objects.filter(category = self).count()
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+    ("Active","Active"),
+    ("Disabled","Disabled"),
+    ("Draft","Draft")
+    )
+
+    user = models.ForeignKey(to=User,on_delete=models.CASCADE)
+    profile = models.ForeignKey(to=Profile,on_delete=models.CASCADE,blank=True,null=True)
+    category = models.ForeignKey(to=Category,blank=True,null=True,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True,blank=True)
+    image = models.FileField(upload_to="image",null=True,blank=True)
+    status = models.CharField(choices=STATUS_CHOICES,max_length=100,default="Active")
+    views = models.IntegerField(default=0)
+    likes = models.ManyToManyField(to=User,blank=True,related_name="likes_user")
+    date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True,null=True,blank=True)
+
+
+    class Meta:
+        ordering = ["-date"]
+
+    def __str__(self):
+        return self.title
+    
+    def save(self,*args, **kwargs):
+        if self.slug=="" or self.slug==None:
+            self.slug = slugify(self.title) + "-" + shortuuid.uuid()[:2]
+
+        super(Post,self).save(*args, **kwargs)
+
+class Comment(models.Model):
+    post = models.ForeignKey(to=Post,on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    comment = models.TextField(null=True,blank=True)
+    reply = models.TextField(null=True,blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-date"]
+
+    def __str__(self):
+        return self.post.title
+    
+class Bookmark(models.Model):
+    user = models.ForeignKey(to=User,on_delete=models.CASCADE)
+    post = models.ForeignKey(to=Post,on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ["-date"]
+
+    def __str__(self):
+        return self.post.title
+
+class Notification(models.Model):
+    NOTIFICATION_TYPES = (
+        ("Like","Like"),
+        ("Comment","Comment"),
+        ("Bookmark","Bookmark"),
+    )
+
+    user = models.ForeignKey(to=User,on_delete=models.CASCADE)
+    post = models.ForeignKey(to=Post,on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    type = models.CharField(choices=NOTIFICATION_TYPES, max_length=100)
+    seen = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-date"]
+
+    def __str__(self):
+        if self.post:
+            return f"{self.post.title} - {self.type}"
+        else:
+            return "Notification"
