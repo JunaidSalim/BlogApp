b802205076fea37a8444a317a41a9de70d5a844e
Junaid Saleem <junaidsaleem986@gmail.com>
2024-08-20 12:48:48 +0500

    Dashboard API Stats View

--- backend/api/views.py
+++ backend/api/views.py
@@ -182,4 +182,29 @@ class BookmarkPostAPIView(APIView):
             )
             return Response({"message": "Post Bookmarked"}, status=status.HTTP_201_CREATED)
 
+class DashboardStats(generics.ListAPIView):
+    serializer_class = api_serializer.AuthorSerializer
+    permission_classes = [AllowAny]
 
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']
+        user = api_models.User.objects.get(id=user_id)
+
+        views = api_models.Post.objects.filter(user=user).aggregate(view = Sum("views"))['view']
+        posts = api_models.Post.objects.filter(user=user).count()
+        likes = api_models.Post.objects.filter(user=user).aggregate(total_likes = Sum("likes"))['total_likes']
+        bookmarks = api_models.Bookmark.objects.all().count()
+
+        return [{
+            "views": views,
+            "posts": posts,
+            "likes": likes,
+            "bookmarks": bookmarks,
+        }]
+    
+    def list(self,request,*args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset,many=True)
+        return Response(serializer.data)
+
+    
